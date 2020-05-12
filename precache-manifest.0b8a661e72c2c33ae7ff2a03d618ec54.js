7fe3c82189411c1176adfa997160a0d01c1bfa5f
Yash Mody <yashmody@buffalo.edu>
2020-05-11 20:18:08 -0500

    Updates

---
+++ precache-manifest.0b8a661e72c2c33ae7ff2a03d618ec54.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca4fbfa55ae2d3dde66f8d1e335102fb",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "337e579bf203bea8d252",
+    "url": "/monsters-rolodex/static/css/main.1cacaa0b.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "337e579bf203bea8d252",
+    "url": "/monsters-rolodex/static/js/main.f676611b.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
